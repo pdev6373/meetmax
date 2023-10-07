e86ffce3964aa0c9fe76c37696a74efb7c113b2b
pdev6373 <pdev6373@gmail.com>
2023-10-08 00:33:41 +0100

    finish logout ui

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,64 +1,137 @@
 "use client";
+import { useState } from "react";
 import { SIDEBAR } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.css";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Logo } from "..";
 
 export default function Sidebar() {
   const pathname = usePathname();
+  const router = useRouter();
+  const [showLogout, setShowLogout] = useState(false);
 
   const isCurrentRoute = (route: string) =>
     pathname === route || (route.length > 1 && pathname.startsWith(route));
+  const logoutPopupHandler = () => setShowLogout(true);
+  const cancelLogoutHandler = () => setShowLogout(false);
+  const logoutHandler = async () => {
+    router.replace("/login");
+    setShowLogout(false);
+  };
 
   return (
-    <div className={styles.wrapper}>
-      <Link href="/" className={styles.logo}>
-        <Logo />
-      </Link>
+    <>
+      {showLogout && (
+        <div className={styles.overlay}>
+          <div className={styles.logoutWrapper}>
+            <h3 className={styles.logoutHeading}>Logout</h3>
+            <p className={styles.logoutText}>
+              Are you sure you want to logout?
+            </p>
 
-      <nav className={styles.nav}>
-        <ul className={styles.navList}>
-          {SIDEBAR.map((nav) => (
-            <li key={nav.name} className={styles.sideNavList}>
-              <Link
-                href={nav.route}
-                className={[
-                  styles.navLink,
-                  isCurrentRoute(nav.route) && styles.navLinkActive,
-                ].join(" ")}
+            <div className={styles.logoutActions}>
+              <button
+                className={styles.logoutCancel}
+                onClick={cancelLogoutHandler}
               >
-                <Image
-                  src={nav.icon}
-                  alt="nav icon"
-                  width={14}
-                  height={14}
-                  className={styles.navIconSmall}
-                />
-                <Image
-                  src={nav.icon}
-                  alt="nav icon"
-                  width={16}
-                  height={16}
-                  className={styles.navIcon}
-                />
-                <p
-                  className={[
-                    styles.navText,
-                    isCurrentRoute(nav.route) && styles.navTextActive,
-                  ].join(" ")}
-                >
-                  {nav.name}
-                </p>
-                {nav.name === "Notification" && (
-                  <p className={styles.notificationNumber}>2</p>
+                Cancel
+              </button>
+              <button className={styles.logoutProceed} onClick={logoutHandler}>
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={styles.wrapper}>
+        <Link href="/" className={styles.logo}>
+          <Logo />
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            {SIDEBAR.map((nav) => (
+              <li key={nav.name} className={styles.sideNavList}>
+                {nav.route ? (
+                  <Link
+                    href={nav.route}
+                    className={[
+                      styles.navLink,
+                      isCurrentRoute(nav.route) &&
+                        !showLogout &&
+                        styles.navLinkActive,
+                    ].join(" ")}
+                  >
+                    <Image
+                      src={nav.icon}
+                      alt="nav icon"
+                      width={14}
+                      height={14}
+                      className={styles.navIconSmall}
+                    />
+                    <Image
+                      src={nav.icon}
+                      alt="nav icon"
+                      width={16}
+                      height={16}
+                      className={styles.navIcon}
+                    />
+                    <p
+                      className={[
+                        styles.navText,
+                        isCurrentRoute(nav.route) &&
+                          !showLogout &&
+                          styles.navTextActive,
+                      ].join(" ")}
+                    >
+                      {nav.name}
+                    </p>
+                    {nav.name === "Notification" && (
+                      <p className={styles.notificationNumber}>2</p>
+                    )}
+                  </Link>
+                ) : (
+                  <div
+                    onClick={logoutPopupHandler}
+                    className={[
+                      styles.navLink,
+                      showLogout && styles.navLinkActive,
+                    ].join(" ")}
+                  >
+                    <Image
+                      src={nav.icon}
+                      alt="nav icon"
+                      width={14}
+                      height={14}
+                      className={styles.navIconSmall}
+                    />
+                    <Image
+                      src={nav.icon}
+                      alt="nav icon"
+                      width={16}
+                      height={16}
+                      className={styles.navIcon}
+                    />
+                    <p
+                      className={[
+                        styles.navText,
+                        showLogout && styles.navTextActive,
+                      ].join(" ")}
+                    >
+                      {nav.name}
+                    </p>
+                    {nav.name === "Notification" && (
+                      <p className={styles.notificationNumber}>2</p>
+                    )}
+                  </div>
                 )}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
--- constants/index.tsx
+++ constants/index.tsx
@@ -36,7 +36,7 @@ export const SIDEBAR = [
   },
   {
     name: "Logout",
-    route: "/logout",
+    route: null,
     icon: "/assets/logout.svg",
   },
 ];
