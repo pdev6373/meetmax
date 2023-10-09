3808d0e917c5e7a44fbb2416a7f12f571ba3b7df
pdev6373 <pdev6373@gmail.com>
2023-10-09 19:48:31 +0100

    finish settings notification ui

--- app/[locale]/(main)/(mainWithoutAside)/settings/notification/page.tsx
+++ app/[locale]/(main)/(mainWithoutAside)/settings/notification/page.tsx
@@ -1,3 +1,74 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { SettingsHeading, SettingsRouteText, Switch } from "@/components";
+import styles from "./page.module.css";
+
 export default function Notification() {
-  return <div>page</div>;
+  const [notifications, setNotifications] = useState([
+    {
+      icon: "/assets/like-notification.svg",
+      name: "Like",
+      isActive: true,
+    },
+    {
+      icon: "/assets/comment-notification.svg",
+      name: "Comment",
+      isActive: true,
+    },
+    {
+      icon: "/assets/follow.svg",
+      name: "Mention",
+      isActive: true,
+    },
+    {
+      icon: "/assets/post.svg",
+      name: "Post",
+      isActive: false,
+    },
+    {
+      icon: "/assets/follow.svg",
+      name: "Follow",
+      isActive: false,
+    },
+  ]);
+
+  return (
+    <>
+      <SettingsRouteText>Notifications</SettingsRouteText>
+      <SettingsHeading>Notifications</SettingsHeading>
+      <p className={styles.heading}>What Notifications You Receive</p>
+      <div className={styles.notifications}>
+        {notifications.map((notification) => (
+          <div className={styles.notification}>
+            <div className={styles.notificationMain}>
+              <Image
+                src={notification.icon}
+                alt="notification icon"
+                width={16}
+                height={16}
+              />
+              <p className={styles.notificationText}>{notification.name}</p>
+            </div>
+
+            <Switch
+              onClick={() =>
+                setNotifications((prev) =>
+                  prev.map((prevNotification) =>
+                    prevNotification.name !== notification.name
+                      ? prevNotification
+                      : {
+                          ...prevNotification,
+                          isActive: !notification.isActive,
+                        }
+                  )
+                )
+              }
+              state={notification.isActive ? "on" : "off"}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
