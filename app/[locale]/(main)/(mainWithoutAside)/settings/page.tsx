fe157defa3b29b5bdc99b0e1416c444eda302e2c
pdev6373 <pdev6373@gmail.com>
2023-10-28 02:41:21 +0100

    finish adding languages

--- app/[locale]/(main)/(mainWithoutAside)/settings/page.tsx
+++ app/[locale]/(main)/(mainWithoutAside)/settings/page.tsx
@@ -6,11 +6,33 @@ export default function Settings() {
 
   return (
     <EditProfile
+      editProfile={t("editProfile")}
+      fullName={t("fullName")}
+      birthday={t("birthday")}
+      bio={t("bio")}
+      enterYourBio={t("enterYourBio")}
+      enterYourLocation={t("enterYourLocation")}
+      locationText={t("location")}
+      defaultError={t("required")}
+      websiteText={t("website")}
+      enterYourWebsite={t("enterYourWebsite")}
+      enterYourPhoneNumber={t("enterYourPhoneNumber")}
+      phoneNumberError={t("phoneNumberError")}
+      websiteError={t("websiteError")}
+      phoneNumberText={t("phoneNumber")}
+      yourUsername={t("yourUsername")}
+      socialLink={t("socialLink")}
+      linkError={t("linkError")}
+      namesError={t("namesError")}
+      successText={t("profileUpdateSuccess")}
+      anErrorOccurred={t("anErrorOccurred")}
+      genderText={t("gender")}
+      facebookText={t("facebook")}
+      linkedinText={t("linkedin")}
+      instagramText={t("instagram")}
+      twitterText={t("twitter")}
       male={t("male")}
       female={t("female")}
-      defaultError={t("required")}
-      emailError={t("emailError")}
-      namesError={t("namesError")}
       save={t("save")}
       cancel={t("cancel")}
     />
